5f66bf90c59a8046db57b1d5740f68dad9edfda5
liuzihao <liuzihao@me>
2021-09-15 09:49:52 +0800

    提交

--- src/main/java/alg/tooffer/Demo55.java
+++ src/main/java/alg/tooffer/Demo55.java
@@ -1,8 +1,22 @@
 package alg.tooffer;
 
+import datastructure.tree.Tnode;
+import org.junit.Test;
+
 /**
  * @author liuzihao
  * @create 2021-09-03-08:38
+ * 二叉树的深度
+ * 输入一颗二叉树的根节点，求二叉树的深度
  */
-public class Demo55 {
+public class Demo55 extends DemoBase{
+
+    /**
+     * 解题思路
+     */
+    @Test
+    public void test(){
+        // 初始化一个默认的树
+        Tnode root = InitializeTree();
+    }
 }
