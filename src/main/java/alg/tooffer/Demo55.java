2467552f5ac00cc0f0ad9699ca2908911a4c08e4
liuzihao <liuzihao@me>
2021-09-15 10:11:07 +0800

    53: 树的深度求解

--- src/main/java/alg/tooffer/Demo55.java
+++ src/main/java/alg/tooffer/Demo55.java
@@ -13,10 +13,24 @@ public class Demo55 extends DemoBase{
 
     /**
      * 解题思路
+     * 当前最大深度h = max( h左子树, h右子树) + 1(当前节点)
+     * 递归求解答；
      */
     @Test
     public void test(){
         // 初始化一个默认的树
         Tnode root = InitializeTree();
+        int a = getDepth(root);
+        System.out.println("当前树的深度为："+a);
+    }
+
+
+    private int getDepth(Tnode tnode) {
+        // 递归出口
+        if ( null == tnode) {
+            return 0;
+        }
+        // max( h左子树, h右子树) + 1(当前节点)
+        return getDepth(tnode.lchild)>getDepth(tnode.rchild) ? getDepth(tnode.lchild)+1 : getDepth(tnode.rchild)+1;
     }
 }
